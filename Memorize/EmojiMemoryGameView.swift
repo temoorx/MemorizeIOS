3402df7cb97169da284ba6df40e65571b5d14108
temoorx <taak140@gmail.com>
2024-06-27 17:38:10 +0500

    custom aspectVGrid

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -8,65 +8,66 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
+    
     @ObservedObject var viewModel: EmojisMemoryGame
+    
+    private let aspectRatio: CGFloat = 2/3
+    
     var body: some View {
         
         VStack {
             Text("Memorize!")
                 .font(.largeTitle)
-            ScrollView{
-                    cards
-                    .animation(.default, value: viewModel.cards)
-            }    .padding()
+            
+            cards
+                .animation(.default, value: viewModel.cards)
+                .padding()
             Button("Shuffle"){
                 viewModel.shuffle()
             }
-        
+            
         }
     }
     
-    var cards : some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards){
-                card in CardView(card)
-                    .aspectRatio(2/3,contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
-            }
-        }
-        .foregroundColor(.orange)
+    private var cards : some View {
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) {
+            card in CardView(card)
+                .aspectRatio(aspectRatio ,contentMode: .fit)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
+                }
+            
+        }.foregroundColor(.orange)
     }
     
-}
-
-
-struct CardView: View {
-    let card: MemorizeGame<String>.Card
-    init( _ card: MemorizeGame<String>.Card) {
-        self.card = card
-    }
-    var body: some View {
-        ZStack{
-            let base = RoundedRectangle(cornerRadius: 12)
-            Group{
-                base.foregroundColor(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(card.content)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.1)
-                    .aspectRatio(1, contentMode: .fit)
-            }
-            .opacity(card.isFaceUp ? 1 : 0)
-            base.fill()
-                .opacity(card.isFaceUp ? 0 : 1)
+    
+    
+    struct CardView: View {
+        let card: MemorizeGame<String>.Card
+        init( _ card: MemorizeGame<String>.Card) {
+            self.card = card
         }
-        .opacity(card.isFaceUp || !card.isMatched ? 1: 0)
-        
+        var body: some View {
+            ZStack{
+                let base = RoundedRectangle(cornerRadius: 12)
+                Group{
+                    base.foregroundColor(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(card.content)
+                        .font(.system(size: 200))
+                        .minimumScaleFactor(0.1)
+                        .aspectRatio(1, contentMode: .fit)
+                }
+                .opacity(card.isFaceUp ? 1 : 0)
+                base.fill()
+                    .opacity(card.isFaceUp ? 0 : 1)
+            }
+            .opacity(card.isFaceUp || !card.isMatched ? 1: 0)
+            
+        }
     }
 }
-
 
 #Preview {
     EmojiMemoryGameView(
