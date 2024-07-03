df3047f7b17ada5638da3e01c2abffd516892d13
temoorx <taak140@gmail.com>
2024-07-03 14:58:48 +0500

    card flying animation added

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -18,26 +18,39 @@ struct CardView: View {
    
     var body: some View {
         ZStack{
-            
-                Pie(endAngle: .degrees(180))
-                    .opacity(0.4)
-                    .overlay(Text(card.content)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.1)
-                        .aspectRatio(1, contentMode: .fit))
-                        .padding(5)
-                        .rotationEffect(.degrees(card.isMatched ? 360:0))
-                        .animation(.spin(duration: 1), value: card.isMatched)
+            TimelineView(.animation) { timeline in
+                if card.isFaceUp || !card.isMatched {
+                    Pie(endAngle: .degrees(card.bonusPercentRemaining * 360))
+                            .opacity(0.4)
+                            .overlay{
+                                cardContents.padding(5)
+                            }
+                            .padding(5)
+                            .cardify(isFaceUp: card.isFaceUp)
+                            .transition(.opacity)
+                } else{
+                    Color.clear
+                }
+            }
+           
                 
             
         }
-        .padding(5)
-        .cardify(isFaceUp: card.isFaceUp)
-        .opacity(card.isFaceUp || !card.isMatched ? 1: 0)
+        
         
     }
-   
+        var cardContents: some View{
+            Text(card.content)
+                .font(.system(size: 200))
+                .minimumScaleFactor(0.1)
+                .aspectRatio(1, contentMode: .fit)
+                .rotationEffect(.degrees(card.isMatched ? 360:0))
+                .animation(.spin(duration: 1), value: card.isMatched)
+        }
+      
+
     }
+
 
 extension Animation {
     static func spin(duration: TimeInterval) -> Animation{
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,7 +11,11 @@ struct EmojiMemoryGameView: View {
     
     @ObservedObject var viewModel: EmojisMemoryGame
     
+    typealias Card = MemorizeGame<String>.Card
+
     private let aspectRatio: CGFloat = 2/3
+    
+
     
     var body: some View {
         
@@ -43,21 +47,35 @@ struct EmojiMemoryGameView: View {
         AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) {
             card in CardView(card)
                 .aspectRatio(aspectRatio ,contentMode: .fit)
+                .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                .zIndex(scoreChange(causedBy: card) != 0 ? 100: 0)
                 .padding(4)
                 .onTapGesture {
-                    withAnimation(){
-                        viewModel.choose(card)
-                    }
+                    choose(card)
                     
                 }
+        }
             
         }
+        private func choose(_ card: Card){
+            withAnimation(){
+                let scoreBeforeChoosing = viewModel.score
+                viewModel.choose(card)
+                let scoreChange = viewModel.score - scoreBeforeChoosing
+                lastScoreChange = (scoreChange, causedByCardId: card.id)
+            }
+        }
+        
+        @State private var lastScoreChange = (0, causedByCardId: "")
+        
+         private func scoreChange(causedBy card: Card) -> Int{
+            let (amount, id) = lastScoreChange
+            return card.id == id ? amount : 0
+        }
+        
     }
     
     
-    
-
-}
 
 #Preview {
     EmojiMemoryGameView(
